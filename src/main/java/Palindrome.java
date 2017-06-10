9c90a5596f34e7c755b2b5bc8a6ae7e92bb9bc89
Carlos <carlos@localhost.localdomain>
2017-06-10 10:34:45 +0100

    Refactored

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -6,16 +6,19 @@ import java.util.*;
 public class Palindrome {
 
     /**
-     * Find 3 longest unique palindromes in a supplied string
+     * Find first N longest palindromes in a supplied string
      *
      * @param str string to look in
+     * @param n   number of results to find
      * @return set of results
      */
-    public static Set<String> firstThreeLongestPalindromes(String str) {
+    public static Set<String> firstNLongestPalindromes(String str, int n) {
+        if (str == null || n == 0) return Collections.emptySet();
+
         Set<String> results = new HashSet();
         for (int size = str.length(); size > 1; size--) {
-            findPalindromesOfSize(str, size, results);
-            if (results.size() >= 3) return results;
+            findPalindromesOfSize(str, size, results, n);
+            if (results.size() >= n) return results;
         }
         return results;
     }
@@ -24,15 +27,16 @@ public class Palindrome {
      * Find unique palindromes of determined sized in a supplied string
      *
      * @param str     string to look in
-     * @param size    palindrome size
+     * @param size    palindrome size to look for
      * @param results set of results
+     * @param n       number of results to find
      */
-    private static void findPalindromesOfSize(String str, int size, Set<String> results) {
+    private static void findPalindromesOfSize(String str, int size, Set<String> results, int n) {
         for (int pos = 0; (pos + size) <= str.length(); pos++) {
             if (!isContained(str.substring(pos, pos + size), results) && isPalindrome(str.substring(pos, pos + size))) {
                 results.add(str.substring(pos, pos + size));
                 System.out.println("Text: " + str.substring(pos, pos + size) + " , Index: " + pos + ", Length: " + size);
-                if (results.size() >= 3) return;
+                if (results.size() >= n) return;
             }
         }
     }
@@ -44,7 +48,7 @@ public class Palindrome {
      * @param set the set of strings to look in
      * @return
      */
-    private static boolean isContained(String str, Set<String> set) {
+    static boolean isContained(String str, Set<String> set) {
         for (String entry : set) {
             if (entry.contains(str)) return true;
         }
@@ -57,11 +61,11 @@ public class Palindrome {
      * @param str the string
      * @return
      */
-    private static boolean isPalindrome(String str) {
-        if (str.length() == 0 || str.length() == 1) return false;
+    public static boolean isPalindrome(String str) {
+        if (str == null || str.length() == 0 || str.length() == 1) return false;
 
         for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+            if (Character.toLowerCase(str.charAt(i)) != Character.toLowerCase(str.charAt(str.length() - 1 - i))) {
                 return false;
             }
         }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,33 +9,50 @@ import java.util.Set;
 public class Tests {
 
     @Test
+    public void isWordPalindrome() {
+        Assert.assertTrue(Palindrome.isPalindrome("aBa"));
+        Assert.assertTrue(Palindrome.isPalindrome("aBba"));
+        Assert.assertTrue(Palindrome.isPalindrome("aa"));
+        Assert.assertTrue(Palindrome.isPalindrome("aA"));
+        Assert.assertTrue(Palindrome.isPalindrome("Aa"));
+    }
+
+    @Test
+    public void isWordNotPalindrome() {
+        Assert.assertFalse(Palindrome.isPalindrome(null));
+        Assert.assertFalse(Palindrome.isPalindrome(""));
+        Assert.assertFalse(Palindrome.isPalindrome("a"));
+        Assert.assertFalse(Palindrome.isPalindrome("abc"));
+    }
+
+    @Test
     public void findNoPalindrome() {
-        Assert.assertEquals(0, Palindrome.firstThreeLongestPalindromes("asfdasdfasfw").size());
+        Assert.assertEquals(0, Palindrome.firstNLongestPalindromes("asfdasdfasfw", 3).size());
     }
 
     @Test
     public void findNoPalindromeWithEmptyString() {
-        Assert.assertEquals(0, Palindrome.firstThreeLongestPalindromes("").size());
+        Assert.assertEquals(0, Palindrome.firstNLongestPalindromes("", 3).size());
     }
 
     @Test
     public void findOneSinglePalindrome() {
-        Assert.assertEquals(1, Palindrome.firstThreeLongestPalindromes("abba").size());
+        Assert.assertEquals(1, Palindrome.firstNLongestPalindromes("abba", 3).size());
     }
 
     @Test
     public void findOneCenteredPalindrome() {
-        Assert.assertEquals(1, Palindrome.firstThreeLongestPalindromes("abcycba").size());
+        Assert.assertEquals(1, Palindrome.firstNLongestPalindromes("abcycba", 3).size());
     }
 
     @Test
     public void findThreeSinglePalindrome() {
-        Assert.assertEquals(3, Palindrome.firstThreeLongestPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop").size());
+        Assert.assertEquals(3, Palindrome.firstNLongestPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3).size());
     }
 
     @Test
     public void matchThreeSinglePalindrome() {
-        Set<String> results = Palindrome.firstThreeLongestPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop");
+        Set<String> results = Palindrome.firstNLongestPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3);
         Assert.assertTrue(results.contains("hijkllkjih"));
         Assert.assertTrue(results.contains("defggfed"));
         Assert.assertTrue(results.contains("abccba"));
